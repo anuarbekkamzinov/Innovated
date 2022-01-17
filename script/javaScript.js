f09b06e0cb7342e988e58b5b052b32d83f137741
anuarbekkamzinov <anuarbekkamzinov@gmail.com>
2022-01-17 22:32:38 +0600

    сделал

--- script/javaScript.js
+++ script/javaScript.js
@@ -1,22 +1,13 @@
-const swiper = new Swiper('.swiper', {
-    slidesPerView: 1,
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+const swiper = new Swiper('.my-swiper-slider', {
+  slidesPerView: 2,
+  loop: false,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.my-swiper-btn-next',
+    prevEl: '.my-swiper-btn-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+});
